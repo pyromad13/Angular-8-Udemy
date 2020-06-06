793fc84a5fca073db2cd4e2b33d26493472de3fe
pyromalis@gmail.com <oldholborn1988>
2020-06-06 23:35:48 +0100

    Menu book refactor init commit

--- menubook/src/app/app.component.spec.ts
+++ menubook/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'menuApp'`, () => {
+  it(`should have as title 'menubook'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('menuApp');
+    expect(app.title).toEqual('menubook');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('menuApp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('menubook app is running!');
   });
 });
