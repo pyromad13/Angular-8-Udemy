bf7f0092769f2b4d6430ff1c41911be7e22b1228
pyromalis@gmail.com <oldholborn1988>
2020-06-07 00:05:49 +0100

    add necessary imports

--- menubook/src/app/app.module.ts
+++ menubook/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
